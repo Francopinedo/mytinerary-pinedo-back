58c81001d9a737a8e0a82999679cbff387e9ce6e
Francopinedo <francopinedo23@gmail.com>
2023-08-22 19:11:01 -0300

    V2

---
+++ src/controllers/citiesController.js
@@ -0,0 +1,12 @@
+const getCities = (req, res) => {
+    res.json({
+        cities:[
+            { 
+            tittle: "aaa",
+            info:"info",
+            img:"img"
+             }
+            ]
+    })
+    }
+    module.exports = {getCities}
